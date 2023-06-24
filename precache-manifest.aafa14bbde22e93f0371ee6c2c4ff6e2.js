03b961ef3b416750035f773966d83efe14907827
vikas-saini-7 <vikas289888@gmail.com>
2023-06-24 19:12:01 +0530

    Updates

---
+++ precache-manifest.aafa14bbde22e93f0371ee6c2c4ff6e2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7189124a399eaac06f7854d62ff72640",
+    "url": "/med-assist/index.html"
+  },
+  {
+    "revision": "d8423a3f72fc5a7f916d",
+    "url": "/med-assist/static/css/main.36c5c196.chunk.css"
+  },
+  {
+    "revision": "e9065e510c24fe96f643",
+    "url": "/med-assist/static/js/2.1e4b8d7d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/med-assist/static/js/2.1e4b8d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8423a3f72fc5a7f916d",
+    "url": "/med-assist/static/js/main.b39d0a30.chunk.js"
+  },
+  {
+    "revision": "6ffdcd24f9cd03819e0e",
+    "url": "/med-assist/static/js/runtime-main.3ca46f6a.js"
+  },
+  {
+    "revision": "cfdbafe37f7253f018b83eb689f02d2e",
+    "url": "/med-assist/static/media/cancel.cfdbafe3.png"
+  },
+  {
+    "revision": "114c0496c822d51cef120c15c6b14b4e",
+    "url": "/med-assist/static/media/letter-i.114c0496.png"
+  },
+  {
+    "revision": "0a9b87659985033ae30213c2141fdf6b",
+    "url": "/med-assist/static/media/logo.0a9b8765.png"
+  },
+  {
+    "revision": "7c583c79e665fe726210608b348137e1",
+    "url": "/med-assist/static/media/logo.7c583c79.svg"
+  }
+]);
